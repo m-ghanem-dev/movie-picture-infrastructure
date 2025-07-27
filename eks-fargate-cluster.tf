6c40acce865d2e385d572be423cec93041603e79
m-ghanem-dev <mohamedgh4444@gmail.com>
2025-07-27 23:21:38 +0200

    enable dns support

--- eks-fargate-cluster.tf
+++ eks-fargate-cluster.tf
@@ -13,6 +13,8 @@ resource "aws_eks_cluster" "main" {
     subnet_ids              = [aws_subnet.private.id, aws_subnet.public.id]
     endpoint_public_access  = true
     endpoint_private_access = true
+    enable_dns_support = true
+
   }
 
   depends_on = [
