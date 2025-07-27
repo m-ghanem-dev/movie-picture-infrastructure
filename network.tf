2656e23cc8a4d5e86d09b82a34e85ad1d4b2d181
m-ghanem-dev <mohamedgh4444@gmail.com>
2025-07-27 17:59:17 +0200

    chore: fix az

--- network.tf
+++ network.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                 = aws_vpc.main.id
   cidr_block             = "10.0.1.0/24"
-  availability_zone      = "eu-central-1"
+  availability_zone      = "eu-central-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "kubernetes.io/cluster/movie-picture-eks-cluster"
@@ -25,7 +25,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1a"
   tags = {
     Name = "kubernetes.io/cluster/movie-picture-eks-cluster"
   }
