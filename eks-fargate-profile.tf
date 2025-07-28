907fd231ed9221a25ab037a049c56e2077120362
m-ghanem-dev <mohamedgh4444@gmail.com>
2025-07-28 21:45:01 +0200

    chore: comment out labels

--- eks-fargate-profile.tf
+++ eks-fargate-profile.tf
@@ -30,9 +30,9 @@ resource "aws_eks_fargate_profile" "kube_system" {
   selector {
     namespace = "kube-system"
     # Enable the below labels if we want only CoreDNS Pods to run on Fargate from kube-system namespace
-    labels = { 
-      "k8s-app" = "kube-dns"
-    }
+    #labels = { 
+      #"k8s-app" = "kube-dns"
+    #}
   }
 
   depends_on = [
