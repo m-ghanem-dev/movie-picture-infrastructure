5758bc671e592acc270822fa103ced1689fd2b6f
m-ghanem-dev <mohamedgh4444@gmail.com>
2025-07-29 03:07:40 +0200

    fix

--- eks-fargate-cluster.tf
+++ eks-fargate-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "21.0.4"
 
   cluster_name    = "movie-picture-eks-cluster"
   cluster_version = var.k8s_version
